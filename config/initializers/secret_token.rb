80a23f83cf9a19aaa5df95e1283242574875139e
Ken Lu <ken@seeice.com>
2013-12-28 15:17:08 +0000

    init rails 4.0.2 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reliefinsight::Application.config.secret_key_base = '312704a8a54366db4b7cc4aad3e8b2aee95384752a0751d6435867860f5e122a5c0173b2fb14bbbda353454659b3ac72f89d0a9202982d09718b064ebcb0f120'
